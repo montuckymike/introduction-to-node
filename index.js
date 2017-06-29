6be523e555128ca235d3f284e4057c13d186d80d
montuckymike <mike@kosmojo.com>
2017-06-29 10:50:32 -0600

    Added post route

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/instagrams", function(req, res){
-  res.send("Oh no mine is very different from that");
+  res.send("Oh no mine is Very different from that");
 })
 
-
+app.post("/instagrams", function(req, res){
+  res.send("I call it Exchangogram - post");
+});
 
 var server = app.listen(3000, function(){
   console.log("Listening on port 3000");
