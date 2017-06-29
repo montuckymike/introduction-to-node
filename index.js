c36e3303be6f26ef2e73b2148b6bc23f012d92bc
montuckymike <mike@kosmojo.com>
2017-06-29 13:14:06 -0600

    updated to now allow submissions

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/instagrams", function(req, res){
 
 app.post("/instagrams", function(req, res){
 
-  var newGram = {title: req.body.title, img: req.body.image, date: makeDate(0) };
+  var newGram = {title: req.body.title, img: req.body.img, date: makeDate(0) };
   instagrams.push(newGram);
 
   res.json({message: "Successfully added Data", data: instagrams });
